cbce0098ca017214bfe8f0f4720478d3fe6bfb6f
Mark Foster <markfoster314@yahoo.com>
2020-01-06 09:35:48 -0500

    Nintendo CSV

--- scraper.py
+++ scraper.py
@@ -4,28 +4,25 @@ from bs4 import BeautifulSoup
 
 
 def scrape_pricecharting(console, game):
-    """Scrapes game market value from pricecharting.com
+    """Scrapes game market values from pricecharting.com
 
     Args:
       console: game system name
       game:    title of video game
 
     Returns:
-      list of the cartridges values
+      list of floats representing game prices [cartridge, CIB, sealed, graded, box, manual]
     """
+
     page = requests.get("https://www.pricecharting.com/game/" + console + "/" + game)
     soup = BeautifulSoup(page.content, 'html.parser')
-
     allPrices = list(list(list(soup.children)[15])[1])
-    cartPrice = float(re.findall("\d+\.\d+", str(list(list(allPrices[1])[1])[0].encode('utf-8')))[0])
-    completePrice = float(re.findall("\d+\.\d+", str(list(list(allPrices[3])[1])[0].encode('utf-8')))[0])
-    sealedPrice = float(re.findall("\d+\.\d+", str(list(list(allPrices[5])[1])[0].encode('utf-8')))[0])
-    gradedPrice = float(re.findall("\d+\.\d+", str(list(list(allPrices[7])[1])[0].encode('utf-8')))[0])
-    boxPrice = float(re.findall("\d+\.\d+", str(list(list(allPrices[9])[1])[0].encode('utf-8')))[0])
-    manualPrice = float(re.findall("\d+\.\d+", str(list(list(allPrices[11])[1])[0].encode('utf-8')))[0])
+    return [float(re.findall("\d+\.\d+", str(list(list(allPrices[2 * i + 1])[1])[0].encode('utf-8')))[0]) for i in range(6)]
 
 def main():
-    scrape_pricecharting("nes", "super-mario-bros-3")
+    consoles = ["nes", "super-nintendo", "nintendo-64", "gamecube", "wii", "wii-u", "nintendo-switch", "gameboy", 
+      "gameboy-color", "gameboy-advance", "nintendo-ds", "nintendo-3ds", "virtual-boy", "game-&-watch"]
+    priceArr = scrape_pricecharting(consoles[0], "super-mario-bros-3")
 
 if __name__== "__main__":
     main()
