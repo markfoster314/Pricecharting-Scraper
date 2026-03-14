7c27befe83db2eddffbc2cca81190519e8c05fff
Scott Longstreet <slongstreet@hotmail.com>
2026-03-14 11:41:31 -0500

    added make db target to generate local sqlite db from latest output

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: install run clean
+.PHONY: install run clean db
 
 venv/bin/activate: requirements.txt
 	python3 -m venv venv
@@ -9,6 +9,9 @@ install: venv/bin/activate
 
 run: venv/bin/activate
 	./venv/bin/python scraper.py
+
+db: venv/bin/activate
+	./venv/bin/python create_db.py
 
 clean:
 	rm -rf venv __pycache__ [0-9][0-9].[0-9][0-9].[0-9][0-9][0-9][0-9]_[0-9][0-9].[0-9][0-9].[0-9][0-9]
--- create_db.py
+++ create_db.py
@@ -2,11 +2,11 @@ import pandas as pd
 import sqlite3
 
 # Load from CSV (or JSON with pd.read_json)
-df = pd.read_csv('gamegear_prices.csv')
+df = pd.read_csv('output-latest/game_prices.csv')
 # Clean prices: Remove '$' and convert to float
 #for col in ['Loose Price', 'CIB Price', 'New Price']:
 #    df[col] = df[col].str.replace('$', '').str.replace(',', '').astype(float)
 
-conn = sqlite3.connect('gameprices.db')  # Creates local DB file
+conn = sqlite3.connect('output-latest/gameprices.db')  # Creates local DB file
 df.to_sql('games', conn, if_exists='replace', index=False)
 conn.close()
